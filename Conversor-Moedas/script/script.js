3f2ca147ea42c6ed03b037f7d55e243856ec9bac
Caio_Carmo <caio.carmo0521@gmail.com>
2023-10-30 17:09:14 -0300

    Melhorando o JS

--- Conversor-Moedas/script/script.js
+++ Conversor-Moedas/script/script.js
@@ -1,21 +1,31 @@
+/*
+    en-US -> USD -> Dolar
+
+    de-DE -> EUR -> Euro
+
+    en-GB -> GBP -> Libra
+
+    pt-BR -> BRL -> Real
+*/
 
 
-
-
+const currencyValue = document.getElementById("currency-value")
+const currencyResultValue = document.getElementById("currency-result-value")
 
 function result() {
     const currencyConvert = document.getElementById("select1").value
     const currencyFor = document.getElementById("select2").value
     const currencyInput = document.getElementById("result").value
-    const currencyValue = document.getElementById("currency-value")
     let currencyDolar = 5
    
     if(currencyConvert == "real" && currencyFor == "dolar") {
         let currencyResult = currencyInput / currencyDolar
-        console.log(currencyResult)
+        // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat (USADO PARA FORMATAR AS MOEDAS)
+        currencyResultValue.innerHTML = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(currencyResult)
     }
-
-    currencyValue.innerHTML  = currencyInput
-
+    currencyValue.innerHTML  = new Intl.NumberFormat ('pt-BR', {style: 'currency', currency: 'BRL'}).format(currencyInput)
 }   
-4
+
+function result() {
+    
+}
